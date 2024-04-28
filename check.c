251e466cf88871019f72dae19b2e17c9eb89285a
Ravi Chaudhary <ravichaudhary9580@outlook.com>
2024-04-28 13:22:01 +0530

    hekki

---
+++ check.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    float a=5;
+    float b=10;
+    int sum;
+    sum=a+b;
+    printf("%d\n",sum);
+        printf("%f\n",sum);
+            printf("%c",sum);
+    return 0;
+}
